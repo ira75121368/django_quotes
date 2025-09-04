5c179dc7e082a026be1e17dd332b40889e0d3461
ira75121368 <ira75121368@gmail.com>
2025-09-04 10:10:53 +0300

    commit with db

--- sitequotes/quotes/models.py
+++ sitequotes/quotes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=255, db_index=True, verbose_name='Название категории')
+
+    def __str__(self):
+        return self.name
+
+
+class Source(models.Model):
+    name = models.CharField(max_length=255, db_index=True, verbose_name='Название источника')
+    category = models.ForeignKey(Categories, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.name} - {self.category.name}'
+
+
+class Quetes(models.Model):
+    text_quotes = models.TextField(unique=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    weight = models.IntegerField(default=1)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+    source = models.ForeignKey(Source, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.text_quotes
