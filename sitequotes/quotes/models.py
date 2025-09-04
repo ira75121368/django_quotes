d96ed84118e874c01da02f76deef8596aeb99b34
ira75121368 <ira75121368@gmail.com>
2025-09-04 19:13:02 +0300

    commit unique_quote

--- sitequotes/quotes/models.py
+++ sitequotes/quotes/models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+import re
 
 
 class Categories(models.Model):
@@ -11,6 +12,14 @@ class Categories(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def normalized_text(text: str) -> str:
+    """ Приводит цитату в определенное состояние для исключения от дубликатов """
+    text = text.lower()
+    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r'\s+', ' ', text).split()
+    return text
 
 
 class Source(models.Model):
@@ -27,6 +36,7 @@ class Source(models.Model):
 
 class Quotes(models.Model):
     text_quotes = models.TextField(unique=True, verbose_name='Цитата')
+    normalize_text = models.TextField(unique=True, editable=False)
     time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     weight = models.IntegerField(default=1, verbose_name='Вес')
     likes = models.PositiveIntegerField(default=0, verbose_name='Лайки')
@@ -42,8 +52,17 @@ class Quotes(models.Model):
         return self.text_quotes
 
     def clean(self):
+        """ Проверка на уникальность + 'не более трёх' """
+        self.normalize_text = normalized_text(self.text_quotes)
+
         if self.source and self.source.quotes_set.count() >= 3:
             raise ValidationError('У этого источника уже есть 3 цитаты')
+
+    def validate_unique(self, exclude=None):
+        super().validate_unique(exclude)
+        if Quotes.objects.exclude(pk=self.pk).filter(normalize_text=self.normalize_text).exists():
+            raise ValidationError({"text_quotes": "Такая цитата уже существует."})
+
 
     def save(self, *args, **kwargs):
         self.full_clean()
